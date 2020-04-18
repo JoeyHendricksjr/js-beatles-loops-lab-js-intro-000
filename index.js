a35f7bccfc8d65f6e1334c7a257a12b5a6cb88e3
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-18 06:23:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let beatlesArr = [];
+  for(let i = 0; i < musicians.length; i++) {
+    //if(musicians[i] === 0 && instruments[i] === 0) {
+      beatlesArr += 'John Lennon plays guitar';
+    //}
+  }
+}
